625b53508816d36c5b1039ca34087f7112933bcb
Nemo <lil_puppy@163.com>
2018-04-19 09:09:01 +0800

    修复scarf的bug（改变border-radius的顺序）
    Signed-off-by: Nemo <lil_puppy@163.com>

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 var text = `
-/*大家好~今天来做点什么呢~
+/*大家好~来做点什么呢~
 先来个背景吧~*/ 
 body{
     background: linear-gradient(to bottom, #bbebf5 0%,#ffffff 76%,#ffffff 76%);
 }
-
+/*调下位置*/
 #code{  
     padding: 16px;
 }
 
-
+/*画个头*/
 .stan .head{
     background-color: #FEDDB2;
+    border-radius: 50%;
 }
+/*加上眼睛*/
 .stan .head .lefteye{
     background-color: #fff;
     border-radius: 50%;
@@ -23,22 +25,23 @@ body{
     border-radius: 50%;
     transform:rotate(-30deg);
 }
+/*加上眼珠*/
 .stan .head .lefteye::before{
-
     background: #000;
-
     border-radius: 50%;
 }
 .stan .head .righteye::before{
     background: #000;
     border-radius: 50%;
 }
+/*嘴巴:)*/
 .stan .head .mouth{
     border: 1px solid transparent;
     border-top: 1px solid #333333;
     border-radius: 50%;
     transform: rotate(180deg);
 }
+/*戴上帽子*/
 .stan .head::before{
     border-top: 70px solid #5162AA;
     border-radius: inherit;
@@ -47,7 +50,7 @@ body{
     box-shadow: 0 -20px 0 -1px #E33940;
     border-radius: 50% 50% 0 0 / 10% 10% 0 0;
 }
-
+/*小绒球*/
 .stan .bobble >span{
     background: #E33940;
     border-radius: 10%;
@@ -70,19 +73,21 @@ body{
 .stan .bobble >span.rect6{
     transform:rotate(154deg);
 }
-
+/*给stan加个身子*/
 .stan .body .inner {
     border-bottom: 104px solid #BB6F4F;
+    border-radius: 10% 21% 50% 50% / 0 0 5% 5%;
     border-left: 30px solid transparent;
     border-right: 30px solid transparent;
-    border-radius: 10% 21% 50% 50% / 0 0 5% 5%;
     transform: translate(-50%,-8px);
     z-index: -1;
 }
+/*穿上衣服*/
 .stan .body .inner::before {
     border-left: 3px solid #333333;
     transform:translateY(12px);
 }
+/*胳膊+手*/
 .stan .body .leftarm {
     background: #BB6F4F;
     transform: skew(-17deg);
@@ -111,11 +116,13 @@ body{
     transform: rotateX(-30deg) skewX(-14deg);
     border-radius: 50% 50% 60% 40% / 20% 50% 50% 20%;
 }
+/*戴上围巾*/
 .stan .body .scarf {
+    z-index: -1;
     background: #E1383B;
     border-radius: 50% 50% 50% 50% / 0 0 85% 85%;
-    z-index: -1;
 }
+/*系好口子*/
 .stan .buttons {
     background: #333333;
     border-radius: 50%;
@@ -129,26 +136,29 @@ body{
     border-radius: inherit;
     background: inherit;
 }
+/*画上腿*/
 .stan .legs{
     z-index: -2;
     background: #5060AB;
 }
 .stan .legs .foot {
     z-index: 1;
-    background: #333333;
     border-radius: 50% 50% 0 0 / 95% 95% 0 0;
+    background: #333333;
 }
 .stan .legs .foot.left {
-    transform:translateX(-8px);
+    transform:translateX(-10px);
 }
 .stan .legs .foot.right {
-    transform:translateX(55px);
+    transform:translateX(58px);
 }`
 var text2=`
 .southPark{
-    transform: rotate(363deg) translateX(-380px) translateY(390px);
+    transform:translateX(-440px) translateY(390px);
 }
+/*现在你有了一个stan~~~*/
 `
+
 
 document.getElementById("mp3").volume = 0.2;
 
@@ -167,7 +177,7 @@ function writeCode(prefix,text,fn) {
             window.clearInterval(id)
             fn.call()
         }
-    }, 30)
+    }, 20)
 }
 
 
